62843787bcdabd1fb923c13c4c8b70e1e98b0444
Asuka165 <asukavalley@gmail.com>
2018-08-01 12:47:10 -0400

    Create EscapeQuotes.php

---
+++ EscapeQuotes.php
@@ -0,0 +1,14 @@
+	/**
+	 *	Function for replacing quotes which can break the code
+	 *
+	 *	Character like single quote, double quote will be replace with ASCII code for HTML
+	 */
+  function EscapeQuote($txt) { 
+      if(is_array($txt)){return array_map(__METHOD__, $txt);}
+
+      if(!empty($txt) && is_string($txt)) { 
+        return str_replace(array("'", '"'), array('&#39;', '&#34;'), $txt); 
+      } 
+
+      return $txt; 
+  }
